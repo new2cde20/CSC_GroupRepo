24469d9c297d7df9bd2267c9876411c220d32f3c
unknown <beingstrong40@gmail.com>
2020-11-30 23:40:33 -0600

    Modified multiple pages that were HTML and PHP

--- newsletters.php
+++ newsletters.php
@@ -10,7 +10,7 @@
             $message = $_POST['message'];
             $antiSpam = $_POST['human'];
         
-            $to ="beingstrong40@gmail.com";
+            $to =""; // My email was here
             $body = '';
         
             $body .="From: ".$userName. "\r\n";
@@ -36,7 +36,6 @@
         <title>Contact Us</title>
         <link rel= "stylesheet" href= "css\style.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat&family=Overpass&display=swap">
-        <!-- <script src="js/cancersite.js"></script> -->
         <script src="js/headerandfooter.js"></script>
     </head>
     <body>
@@ -60,28 +59,28 @@
                 <div class="form-group">
                     <label for="name" class="form-label">Your Name</label>
                     <input type="text" class="form-control" id="name" 
-                    name="name" placeholder="Name" tabindex="1" required>
+                    name="name" placeholder="Your Name*" tabindex="1" required>
                     
                 </div>
                 <div class="form-group">
                     <label for="email" class="form-label">Your Email</label>
                     <input type="email" class="form-control" id="email" 
-                    name="email" placeholder="Email" tabindex="2" required>
+                    name="email" placeholder="Your Email*" tabindex="2" required>
                 </div>
                 <div class="form-group">
                     <label for="subject" class="form-label">Subject</label>
                     <input type="text" class="form-control" id="subject" 
-                    name="subject" placeholder="Subject" tabindex="3" required>
+                    name="subject" placeholder="Subject*" tabindex="3" required>
                 </div>
                 <div class="form-group">
                     <label for="message" class="form-label">Message</label>
                     <textarea class="form-control" rows="5" cols="50" id="message" 
-                    name="message" placeholder="Enter Message..." tabindex="4"></textarea>
+                    name="message" placeholder="Enter Message...*" tabindex="4" required></textarea>
                 </div>
                 <div class="form-group">
                 <label for="human" class="form-label">Enter the answer</label>
                 <input type="text" class="form-control" id="human" 
-                name="human" placeholder="What is 2+2?" tabindex="5" required>
+                name="human" placeholder="What is 2+2?*" tabindex="5" required>
             </div>
                 <div>
                     <button type="submit" class="btn">Send Message!</button>
