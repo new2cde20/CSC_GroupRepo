00e46156b4b61ef0bf88057217efacd1ca3afe23
unknown <beingstrong40@gmail.com>
2020-12-12 16:59:29 -0600

    Updated more files

--- js/headerandfooter.js
+++ js/headerandfooter.js
@@ -148,7 +148,7 @@ class Footer extends HTMLElement {
                     <a href="2020FallUnGala.html" style="color: #f9f9f9; text-decoration:none;">2020 Fall Un-Gala</a><br>
                     <a href="thirdPartyEvents.html" style="color: #f9f9f9; text-decoration:none;">Third Party Community Events</a><br>
                     <a href="takeABiteOutOfCancer.html" style="color: #f9f9f9; text-decoration:none;">Take a Bite Out of Cancer</a>
-                    <a href="2020-Trees-Wreath-Garland-sale.html" style="color: #f9f9f9; text-decoration:none;">2020 Trees, Wreath, and Garland Sale</a><br>
+                    <a href="2020-Trees-Wreath-Garland-sale.html" style="color: #f9f9f9; text-decoration:none;">2020 Trees,<br> Wreath, and Garland Sale</a><br>
                     <a href="givingThanks.html" style="color: #f9f9f9; text-decoration:none;">#GivingTuesday</a>
                 </p>
             </div>
@@ -186,6 +186,7 @@ class Footer extends HTMLElement {
                     <img src="css/mainpage-other-imgs/YouTube-social-square_red_48px.png" width="36px" height="36px" margin: 5px alt="YouTube"></a>
             </div>
         </div>
+    </div>
     `;
     }
 }
